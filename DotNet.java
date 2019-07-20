3fe3e27ccdb9839accc51049ce418365675da9bd
mohankrishna23 <53111632+mohankrishna23@users.noreply.github.com>
2019-07-20 15:07:20 +0530

    Add files via upload

---
+++ DotNet.java
@@ -0,0 +1,35 @@
+package mohan;
+
+public class DotNet {
+	
+	public String name;
+	public int id;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		
+		DotNet value = new DotNet();
+		 
+	value.setName("Mohan");
+	value.setId(77);
+	System.out.println(value.getName());
+	System.out.println(value.getId());
+	}
+
+}
---
+++ Java.java
@@ -0,0 +1,10 @@
+package mohan;
+
+public class Java {
+
+	public static void main(String[] args) {
+	System.out.println("hello Mohan");
+
+	}
+
+}
